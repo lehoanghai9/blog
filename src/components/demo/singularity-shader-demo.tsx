c670e506bfc0a153a883d5270016b7ec944ef58c
lehoanghai9 <hyperminister@gmail.com>
2025-11-13 11:46:40 +0100

    Add SingularityShaderDemo component: Implemented a new demo component for singularity shaders with theme-based properties for speed, intensity, size, wave strength, and color shift.

---
+++ src/components/demo/singularity-shader-demo.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useTheme } from "next-themes";
+import { SingularityShaders } from "../ui/shadcn-io/singularity-shaders";
+
+export default function SingularityShaderDemo({
+  className,
+}: {
+  className?: string;
+}) {
+  const { theme } = useTheme();
+  return (
+    <SingularityShaders
+      speed={1}
+      intensity={theme === "dark" ? 1.7 : 3}
+      size={theme === "dark" ? 1 : 1.1}
+      waveStrength={theme === "dark" ? 1.0 : 0.5}
+      colorShift={theme === "dark" ? 1.5 : 4}
+      className={className}
+    />
+  );
+}
